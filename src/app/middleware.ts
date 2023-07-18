e1c9922491ce3fd583b3873781adacbaac9c1568
Eriquy Jhordan <eriquy@gmail.com>
2023-07-18 11:25:49 -0300

    fix: ip filter

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from 'next/server'
  
 export function middleware(request: NextRequest) {
   const allowedIPs = ['200.150.74.98', '177.39.236.1'];
-  const clientIP = request.headers['x-forwarded-for'] || request.headers["x-real-ip"];
+  const clientIP = request.headers['x-forwarded-for'];
   
   if (!allowedIPs.includes(clientIP)) {
     return NextResponse.redirect(new URL('/404', request.url))
