ef4f96d736f2210f80ffcead17e6f097c2a0ab7c
Eriquy Jhordan <eriquy@gmail.com>
2023-07-18 12:04:28 -0300

    fix: get client ip

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from 'next/server'
  
 export default function middleware(request: NextRequest) {
   const allowedIPs = ['200.150.74.98', '177.39.236.1'];
-  const clientIP = request.headers['x-forwarded-for'];
+  const clientIP = request.headers.get('x-forwarded-for')
   
   if (!allowedIPs.includes(clientIP)) {
     if (request.nextUrl.pathname === '/404') {
