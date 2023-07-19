0ba23d2cdbeb98d7c281f43a1f2fa5d6102ceb29
Eriquy Jhordan <eriquy@gmail.com>
2023-07-19 19:34:22 -0300

    fix: add developer ip

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
  
 export default function middleware(request: NextRequest) {
-  const allowedIPs = ['200.150.74.98', '177.39.236.1'];
+  const allowedIPs = ['200.150.74.98', '177.39.236.1', '2804:d45:e05e:4100:c48f:8fdc:7c3:bd8'];
   const clientIP = request.headers.get('x-forwarded-for')
   
   if (!allowedIPs.includes(clientIP)) {
