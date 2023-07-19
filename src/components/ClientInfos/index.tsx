670d1346002729d5091812baa49ed4b4e5e2b9d3
Eriquy Jhordan <eriquy@gmail.com>
2023-07-19 19:46:11 -0300

    fix: try older version of the calc

--- src/components/ClientInfos/index.tsx
+++ src/components/ClientInfos/index.tsx
@@ -44,16 +44,16 @@ export function ClientInfos() {
         dispatch(updateOperationsWindowValue({
           property: 'receita',
           value: `${(((Number(operation.taxa_final) - Number(operation.spot)) *
-            (Number(operation.volume) -
-            Number(operation.despesas)))/ 2)
+            Number(operation.volume) -
+            Number(operation.despesas))/ 2)
             }`
         }))
       } else {
         dispatch(updateOperationsWindowValue({
           property: 'receita',
           value: `${(((Number(operation.spot) - Number(operation.taxa_final)) *
-            (Number(operation.volume) -
-            Number(operation.despesas)))/2)
+            Number(operation.volume) -
+            Number(operation.despesas))/2)
             }`
         }))
       }
