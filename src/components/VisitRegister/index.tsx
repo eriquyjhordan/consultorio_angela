e376def292039fa83a2c0b9b2725fff954dea976
Eriquy Jhordan <eriquy@gmail.com>
2023-12-24 11:00:26 -0300

    feat: move tmp file to correct path on saving

--- src/components/VisitRegister/index.tsx
+++ src/components/VisitRegister/index.tsx
@@ -1,6 +1,7 @@
 import styles from "./styles.module.sass";
 import { ClientInfos } from "../ClientInfos";
 import { Button, Modal as AntdModal, ConfigProvider } from "antd";
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import {
   resetVisitWindow,
   updateVisitWindowValue,
@@ -12,20 +13,18 @@ import { useEffect, useState } from 'react'
 import { SuccessMessage } from '../ConfimationScreen'
 import VisitDetails from "../VisitDetails";
 import VisitFile from "../visitFile";
+import dayjs from "dayjs";
 
 export default function VisitRegister() {
   const visit = useSelector((state: RootState) => state.visit);
+  const supabase = createClientComponentClient()
   const dispatch = useDispatch();
   const [visitType, setvisitType] = useState<
     "Cadastro" | "Edição" | "Exclusão"
   >("Cadastro");
   const [status, setStatus] = useState<"success" | "error">("success");
 
-  const handleOk = () => {
-    dispatch(
-      updateVisitWindowValue({ property: "isVisitModalOpen", value: false })
-    );
-  };
+  const handleOk = async () => { };
 
   const handleCancel = () => {
     dispatch(
@@ -34,8 +33,66 @@ export default function VisitRegister() {
     dispatch(resetVisitWindow());
   };
 
-    const handleSave = () => {};
-    const handleDelete = () => {};
+  const handleSave = async () => {
+    let fileName = '';
+    if (visit.selectedClient && visit.date) {
+      if (visit.tempFileName) {
+        fileName = `${visit.selectedClient.value}-${visit.date}.${visit.tempFileName.split('.')[1]}`;
+        fileName = fileName.replace(/\s/g, '');
+        const { data, error } = await supabase
+          .storage
+          .from('fichas')
+          .move(`temp/${visit.tempFileName}`, `fichas/${fileName}`)
+        if (error) {
+          setStatus('error')
+          console.log('error: ',error)
+          dispatch(updateVisitWindowValue({ property: "isSuccessScreenOpen", value: true }));
+        }
+      }
+      const { data, error }: any = await supabase.from('visit').insert([
+        {
+          client_id: visit.selectedClient.value,
+          visit_date: dayjs((visit.date).toString(), 'DD-MM-YYYY HH:mm'),
+          lamina_oniciolise: visit.oniciolise,
+          lamina_onicogrifose: visit.onicogrifose,
+          lamina_onicomicose: visit.onicomicose,
+          outras_laminas: visit.outros,
+          observacoes: visit.desconfortoText,
+          visit_record_url:  `https://dhnfwwdtvgnusxvhmbor.supabase.co/storage/v1/object/public/fichas/fichas/${fileName}` 
+        }
+      ]).select()
+      if (data) {
+        setStatus('success')
+        dispatch(updateVisitWindowValue({ property: "isSuccessScreenOpen", value: true }));
+      } else if (error) {
+        setStatus('error')
+        console.log('error: ',error)
+        dispatch(updateVisitWindowValue({ property: "isSuccessScreenOpen", value: true }));
+      }
+    }
+    // dispatch(
+    //   updateVisitWindowValue({ property: "isVisitModalOpen", value: false })
+    // );
+  };
+  const handleDelete = () => { };
+
+  useEffect(() => {
+    async function fetchData() {
+      const { data: visitData, error } = await supabase
+        .from('visit')
+        .select('*')
+        .eq('id', visit.visitId)
+      if (visitData) {
+        dispatch(updateValuesToEdit({
+          date: dayjs(visitData[0].visit_date),
+          ...visitData[0]
+        }))
+      }
+    }
+
+    fetchData()
+
+  }, [visit.isVisitModalOpen, visit.visitId])
 
   return (
     <>
@@ -89,7 +146,6 @@ export default function VisitRegister() {
                     type="primary"
                     className={styles.confirmationBtn}
                   >
-                    {/* {visit.clientId ? "Salvar" : "Cadastrar"} */}
                     Salvar
                   </Button>
                 </div>
